7f3eb45239f1500ef1546ba8c369be9dca3c0c7d
saibia8 <saibia.design@gmail.com>
2022-11-05 14:18:45 +0100

    Work in progress

---
+++ js/question-1.js
@@ -0,0 +1,7 @@
+// Question 1
+// Convert the function below to an arrow function:
+// function getRemainder(a,b) {
+//     return a % b;
+//     }
+    
+const getRemainder = (a, b) => a % b;
---
+++ js/question-2.js
@@ -0,0 +1,38 @@
+const apiKey = "100d2d0c86dc4e65a995a1043e45461f";
+
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${apiKey}`;
+
+const resultsContainer = document.querySelector(".results");
+
+
+async function getGames() {
+
+    try {
+        const response = await fetch(url);
+
+        const results = await response.json();
+    
+        const games = results.results;
+    
+        resultsContainer.innerHTML = "";
+    
+        for (let i = 0; i < games.length; i++) {
+           console.log(games[i]);
+            
+            if (i === 8) {
+                break;
+        }
+    
+           resultsContainer.innerHTML += `<div class="result">
+                                          <div class="title">${games[i].name}</div>
+                                          <div class="rating">${games[i].rating}</div>
+                                          <div>${games[i].tags.length}</div>
+                                          </div>`;
+        }
+    }
+    catch(error) {
+        resultsContainer.innerHTML = displayError("An error occurred when calling the API");
+    }
+}
+
+getGames();
